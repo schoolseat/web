a57b73f1979e0c484c56a9b456a530a0872b1518
Atsushi <88151256+atshushi@users.noreply.github.com>
2021-08-02 14:41:58 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/colors.css';
+import './styles/index.css';
 import './config/i18n';
 import App from './pages/router';
 
