7d9d88d4ab07d2edf1b8ffc9545dedef322124ad
Chronous <lucasmts2017@outlook.com>
2021-08-27 09:44:29 -0300

    ♻️ - Added a PFP dropdown to header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -23,16 +23,29 @@ export default function Header() {
 export function Logged() {
   const { t } = useTranslation();
   return (
-    <div>
-      <a href="/" className="header-texts">
-        {t('navbar:header.home')}
-      </a>
-      <a href="/calendar" className="header-texts">
-        {t('navbar:header.calendar')}
-      </a>
-      <a href="/classes" className="header-texts">
-        {t('navbar:header.classes')}
-      </a>
+    <div className="header-texts-divs">
+      <div>
+        <a href="/" className="header-texts">
+          {t('navbar:header.home')}
+        </a>
+        <a href="/calendar" className="header-texts">
+          {t('navbar:header.calendar')}
+        </a>
+        <a href="/classes" className="header-texts">
+          {t('navbar:header.classes')}
+        </a>
+      </div>
+      <div className="dropdown">
+        <img className="header-pfp" src="https://cdn.discordapp.com/attachments/854896678233702431/880413907095789648/868091706644512820.png" alt="profilePic" />
+        <div className="dropdown-content">
+          <div className="header-dropdown-texts-div">
+            <a href="/profile" className="header-dropdown-texts">{t('navbar:header.profile')}</a>
+          </div>
+          <div className="header-dropdown-texts-div">
+            <a href="/" className="header-dropdown-texts">{t('navbar:header.sing-out')}</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
